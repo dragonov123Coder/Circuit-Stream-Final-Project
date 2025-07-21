bc9c0e33d762d2a0ca294364e650867e3180e513
Viaan Jauhari <viaan.jauhari@gmail.com>
2025-07-21 15:44:57 -0400

    another commit fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+
+const repoName = "Circuit-Stream-Final-Project"; // Change to your repo name if different
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
 };
 
 export default nextConfig;
