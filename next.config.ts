2c4132cce3338a96d2a48ca20d1105b115cd2bab
Viaan Jauhari <viaan.jauhari@gmail.com>
2025-07-22 16:24:18 -0400

    fix c

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-const repoName = "Circuit-Stream-Final-Project";
 const nextConfig: NextConfig = {
-  ...(isProd && {
-    basePath: `/${repoName}`,
-    assetPrefix: `/${repoName}/`,
-  }),
   /* config options here */
 };
 
